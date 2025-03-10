6fb14d7bb4675aabb56211bbf4135ed3127110b3
DavidGomezDelgado <12401dagode@uma.es>
2025-03-10 20:16:45 +0100

    Empty APIREST generated

---
+++ disvise/src/test/java/com/disvise/disvise/DisviseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disvise.disvise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisviseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
